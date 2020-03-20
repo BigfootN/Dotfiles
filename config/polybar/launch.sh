59b0b8428bf6990770dfcc04265a28beb8fe3c2d
bigfoot <bigfoot_arch@protonmail.com>
2020-03-20 21:19:16 +0100

    update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -29,7 +29,7 @@ border = #767676
 
 [section/bar_settings]
 ; height
-height = 2.5%
+height = 2.7%
 
 ; colors
 background = ${colors.bg}
@@ -44,7 +44,8 @@ border-right-color = #2d2d2d
 fixed-center = true
 
 ; font
-font-0 = "Iosevka:size=12;3"
+; font-0 = "Iosevka:size=12;3"
+font-0 = "UbuntuCondensed Nerd Font:size=13;3"
 font-1 = "Iosevka Term:size=18;5"
 font-2 = "Iosevka Term:size=22;5"
 
@@ -60,9 +61,9 @@ override-redirect = true
 
 ; borders size
 border-left-size = 0
-border-right-size = 5
-border-bottom-size = 8
-border-top-size = 8
+border-right-size = 7
+border-bottom-size = 5
+border-top-size = 7
 
 ; modules margin
 module-margin = 1
@@ -74,7 +75,7 @@ wm-restack = i3
 # Bars #
 #------#
 
-[bar/top]
+[bar/main]
 ; top
 bottom = false
 
@@ -82,39 +83,22 @@ bottom = false
 inherit = section/bar_settings
 
 ; geometry
-width = 96.6%
+width = 100%
 
 ; colors
 foreground = #ffffff
 
 ; offset
-offset-y = 10
-offset-x = 1.7%
+offset-y = 0
+offset-x = 0
 
 ; modules
-modules-right = date time
-modules-center = status artist title
+modules-right = wireless-network date time
+modules-center = song
 modules-left = i3
 
 ; language
 locale = fr_FR.UTF-8
-
-[bar/system]
-; bottom
-bottom = true
-
-; inherit
-inherit = section/bar_settings
-
-; modules
-modules-center = updates sound memory cpu wireless-network
-
-; position
-offset-x = 36.7%
-offset-y = 10
-
-; geometry
-width = 26.5%
 
 #---------#
 # Modules #
@@ -169,11 +153,7 @@ type = internal/cpu
 
 format = <label>
 format-prefix = %{O0}
-format-prefix-background = #689d6a
-format-prefix-foreground = #000000
-
-label = %{B#a2a2a2 F#000000}%{O0} %percentage%% %{B- F-}
-inherit = section/base
+format-prefix-background = ${colors.batime
 
 [module/wireless-network]
 type = internal/network
@@ -201,12 +181,12 @@ interval = 1
 
 ; format
 format = <label>
-format-prefix =  %{O0}
-format-prefix-background = #98971a
-format-prefix-foreground = #000000
+format-prefix = 
+format-prefix-background = ${colors.bg}
+format-prefix-foreground = #cc241d
 
 ; label
-label = %{B#a2a2a2 F#000000}%{O-10} %time% %{B- F-}
+label = %{F#cc241d}%time%%{F-}
 
 ; inheritance
 inherit = section/base
@@ -226,19 +206,6 @@ format-prefix-foreground = #000000
 ; label
 label = %{B#a2a2a2 F#000000}%{O-10} %date% %{B- F-}
 format-label-background = #a2a2a2
-
-[module/memory]
-type = internal/memory
-inherit = section/base
-
-; format
-format = <label>
-format-prefix =  %{O0}
-format-prefix-background = #928374
-format-prefix-foreground = #000000
-
-; label
-label = %{B#a2a2a2 F#000000}%{O-10} %percentage_used%% %{B- F-}
 
 [module/artist]
 type = custom/script
@@ -262,6 +229,27 @@ exec = ~/.config/polybar/musicinfo.py song "%a"
 interval = 1
 
 [module/title]
+type = custom/script
+tail = true
+
+; inherit
+inherit = section/base
+
+; format
+format = <label>
+format-prefix =  %{O0}
+format-prefix-background = #d3869b
+format-prefix-foreground = #000000
+
+; label
+label = %{B#a2a2a2 F#000000}%{O-10} %output:0:100% %{B- F-}
+
+; exec
+exec-if = ~/.config/polybar/musicinfo.py song "%t"
+exec = ~/.config/polybar/musicinfo.py song "%t"
+interval = 1
+
+[module/song]
 type = custom/script
 tail = true
 

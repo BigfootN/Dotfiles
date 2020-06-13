5aaddfaf558a6b80a430b978ad3b81df6dc3ca92
BigfootN <bigfoot_arch@protonmail.com>
2020-06-13 12:14:50 +0200

    update

--- utils/dotfiles.sh
+++ utils/dotfiles.sh
@@ -1,14 +1,5 @@
 #!/bin/zsh
 
-declare -A CONFIG_FILES
-CONFIG_FILES["config/clang-format/clang-format"]="~/.config/clang-format/clang-format"
-CONFIG_FILES["config/nvim"]="~/.config/nvim/"
-CONFIG_FILES["config/gtk-3.0/settings.ini"]="~/.config/gtk-3.0/settings.ini"
-CONFIG_FILES["config/gtk-3.0/gtk.css"]="~/.config/gtk-3.0/gtk.css"
-CONFIG_FILES["config/awesome"]="~/.config/awesome"
-CONFIG_FILES["config/picom/picom.conf"]="~/.config/picom/picom.conf"
-CONFIG_FILES["wallpaper/wall.jpg"]="~/.wallpaper/wall.jpg"
-
 copy_from_to() {
 	local path_from=$1
 	local path_to=$2

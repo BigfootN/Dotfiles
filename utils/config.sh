071407b792590cbdc12c2384a703743224fea853
BigfootN <bigfoot_arch@protonmail.com>
2020-08-02 11:03:32 +0200

    update

--- utils/config.sh
+++ utils/config.sh
@@ -37,10 +37,5 @@ configure_nvim() {
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
 
-configure() {
-	configure_fonts
-	configure_xorg
-	configure_grub
-	configure_keyboard
-	configure_clock
-}
+configure_clock
+
